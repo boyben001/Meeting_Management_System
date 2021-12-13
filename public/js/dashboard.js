a1e12056a48c7060d8c027c226d940ccb4a5760f
DESKTOP-BENCHEN\Ben-Chen <boyben001@gmail.com>
2021-12-13 20:12:06 +0800

    feat : month picker && .hide && .highlight

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,2 +1,7 @@
-let today = new Date();
-
+console.log('hello');
+/* Monday 改成 Mon. */
+$("#datepicker").datepicker( {
+    format: "mm-yyyy",
+    startView: "months", 
+    minViewMode: "months"
+});
