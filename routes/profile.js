d78210430cb01ad948c20889ad26cbdfd5701853
PurpleRed <benson060211@gmail.com>
2021-12-19 23:45:14 +0800

    fixed: profile 無法讀取 cookie

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,14 @@ let express = require('express');
 
 let router = express.Router();
 
+/* cookies */
+let cookieParser = require('cookie-parser');
+router.use(cookieParser('PurpleRed is awesome!!'));
+
 router.get('/', (req, res) => {
-    res.render('profile');
+    res.render('profile', {
+        username: req.cookies.username
+    });
 });
 
 module.exports = router;
