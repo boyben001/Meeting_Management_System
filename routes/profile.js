b33be13b42a756a5ea00c90dbf06a7233b7ff6ce
PurpleRed <benson060211@gmail.com>
2021-12-21 16:04:01 +0800

    fixed: profile 相關程式碼精簡化

--- routes/profile.js
+++ routes/profile.js
@@ -2,12 +2,6 @@ let express = require('express');
 
 let router = express.Router();
 
-/* body-parser */
-let bodyParser = require('body-parser');
-let urlencodedParser = bodyParser.urlencoded({
-    extended: false
-});
-
 /* MySQL */
 let mysql = require('mysql');
 let dbOption = {
@@ -17,6 +11,7 @@ let dbOption = {
     password: '0000',
     database: 'Meeting_System_SQL'
 };
+
 /* cookies */
 let cookieParser = require('cookie-parser');
 router.use(cookieParser('PurpleRed is awesome!!'));
@@ -25,30 +20,32 @@ router.get('/', (req, res) => {
     if (req.cookies.username == null) {
         res.redirect('/login');
     } else {
-        let dbConnection = mysql.createConnection(dbOption);
-        function getData(callback){
-            let findUserDataQuery = 'SELECT * FROM 使用者 WHERE 使用者編號 = "' + req.cookies.userID + '"' //透過編號搜尋使用者資料
+        function getUserData(callback) {
+            let dbConnection = mysql.createConnection(dbOption);
+            let findUserDataQuery = 'SELECT * FROM 使用者 WHERE 使用者編號 = "' + req.cookies.userID + '"' // 透過編號搜尋使用者資料
             dbConnection.query(findUserDataQuery, (err, rows, fields) => {
                 if (err) res.render('login', {
                     errmsg: '發生錯誤，請稍後再試一次'
                 });
-                account = rows[0]['帳號'];
-                return callback(account);
+                else {
+                    dbConnection.end();
+                    return callback({
+                        account: rows[0]['帳號'],
+                        identity: rows[0]['身分']
+                    });
+                }
             });
-            dbConnection.end();
+
         }
-        let account = '';
-        getData(function(result){
-            account = result;
-            let renderData = {
+
+        getUserData((userData) => {
+            res.render('profile', {
                 username: req.cookies.username,
-                accountInput: account
-            }
-            res.render('profile', renderData);
+                id: req.cookies.userID,
+                identity: userData['identity'],
+                accountInput: userData['account']
+            });
         });
-        //資料庫抓取資料，跟宏明溝通
-        
-        
     }
 });
 
