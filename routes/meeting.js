14c1f92fb468275c1c82e6645d08a5b1b70f2165
PurpleRed <benson060211@gmail.com>
2022-01-02 00:20:16 +0800

    feat: 建立 /meeting/overview 路由

--- routes/meeting.js
+++ routes/meeting.js
@@ -7,9 +7,23 @@ let cookieParser = require('cookie-parser');
 router.use(cookieParser('PurpleRed is awesome!!'));
 
 router.get('/edit', (req, res) => {
-    res.render('./meeting/edit', {
-        username: req.cookies.username
-    });
+    if (Object.keys(req.cookies).length != process.env.NUM_OF_COOKIES) {
+        res.redirect('/login');
+    } else {
+        res.render('./meeting/edit', {
+            username: req.cookies.username
+        });
+    }
+});
+
+router.get('/overview', (req, res) => {
+    if (Object.keys(req.cookies).length != process.env.NUM_OF_COOKIES) {
+        res.redirect('/login');
+    } else {
+        res.render('./meeting/overview', {
+            username: req.cookies.username
+        });
+    }
 });
 
 module.exports = router;
