a70cc763a96033f6b238fcc90e85c77953ef75eb
YU-HSIU <a1085524@mail.nuk.edu.tw>
2022-01-05 17:36:03 +0800

    feat. meeting葉面人員部分完成

--- public/js/meeting/edit.js
+++ public/js/meeting/edit.js
@@ -1,1 +1,18 @@
-console.log('恭喜你找到ㄌ! 開工囉~');
+window.onload = function() {
+    let form = document.getElementById('authority');
+    for (let i in form.childNodes) {
+        if (i % 2 == 1) {
+            let partYes = form.childNodes[i].childNodes[11].childNodes[1];
+            let partNo = form.childNodes[i].childNodes[11].childNodes[7];
+            let partList = form.childNodes[i].childNodes[11].childNodes[5];
+            partYes.addEventListener('click', func => {
+                if (partList.hasAttribute('disabled'))
+                    partList.removeAttribute('disabled');
+            })
+            partNo.addEventListener('click', func => {
+                partList.setAttribute('disabled', '');
+            })
+        }
+    }
+
+}
