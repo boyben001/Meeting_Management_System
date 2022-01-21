3cda9ead8fe8d650057a98b7a19a0999cc669c20
PurpleRed <benson060211@gmail.com>
2022-01-21 21:51:12 +0800

    feat: /dashboard 日曆版型初始

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,6 @@
-
 $(document).ready(() => {
     /* initialize */
-    
+
     let today = new Date();
     let currentDate; // 當前 datepicker 的值
     $('#datepicker').datepicker({
@@ -14,37 +13,37 @@ $(document).ready(() => {
     currentDate = $('#datepicker').val();
     //setCalendar參數為 => 該月第一天星期幾 , 該月有幾天
     let content = document.getElementById('week_5').innerHTML;
-    setCalendar(new Date(today.getFullYear(),today.getMonth(),1).getDay(),new Date(today.getFullYear(),today.getMonth()+1,0).getDate());
-    function setCalendar(firstDay,dayNum){
+    setCalendar(new Date(today.getFullYear(), today.getMonth(), 1).getDay(), new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate());
+
+    function setCalendar(firstDay, dayNum) {
         let weekNum = 6;
         let week5 = document.getElementById('week_5');
-        if(firstDay+dayNum > 35){                              //若第一天星期幾+該月天數>35 代表需要最後一周 
-            week5.innerHTML = content;                        
-            weekNum = 6;                                       //並調整weekNum(會顯示幾周)
-        }
-        else{                                                  //否則要把最後一周刪掉
+        if (firstDay + dayNum > 35) { //若第一天星期幾+該月天數>35 代表需要最後一周 
+            week5.innerHTML = content;
+            weekNum = 6; //並調整weekNum(會顯示幾周)
+        } else { //否則要把最後一周刪掉
             week5.innerHTML = "";
             weekNum = 5;
         }
-        for(let i=0;i<weekNum;i++){                            //表格初始class內容(清空'hide')
-            for(let j=0;j<7;j++){
+        for (let i = 0; i < weekNum; i++) { //表格初始class內容(清空'hide')
+            for (let j = 0; j < 7; j++) {
                 let block = document.getElementById(i + '-' + j);
-                block.setAttribute('class','card day_block');
+                block.setAttribute('class', 'card day_block');
             }
         }
         let counter = 1;
-        for(let i=0;i<weekNum;i++){                               
-            for(let j=0;j<7;j++){
+        for (let i = 0; i < weekNum; i++) {
+            for (let j = 0; j < 7; j++) {
                 let block = document.getElementById(i + '-' + j);
-                if(i==0 && j<firstDay){                       //前段設成hide
+                if (i == 0 && j < firstDay) { //前段設成hide
                     block.classList.add('hide');
                     continue;
                 }
-                if(counter > dayNum){                         //後段射程hide
+                if (counter > dayNum) { //後段設成hide
                     block.classList.add('hide');
                     continue;
-                }  
-                block.innerHTML = "<span class='number_block'>"+ counter + "</span>";
+                }
+                block.innerHTML = "<span class='number_block'>" + counter + "</span>";
                 counter++;
             }
         }
@@ -55,9 +54,9 @@ $(document).ready(() => {
         let temp = currentDate.split('-');
         let year = Number(temp[0]);
         let month = Number(temp[1]);
-        let firstDay = new Date(year,month-1,1).getDay();   //取得該月第一天星期幾
-        let dayNum = new Date(year,month,0).getDate();      //取得該月有幾天
-        setCalendar(firstDay,dayNum);
+        let firstDay = new Date(year, month - 1, 1).getDay(); //取得該月第一天星期幾
+        let dayNum = new Date(year, month, 0).getDate(); //取得該月有幾天
+        setCalendar(firstDay, dayNum);
         /* TODO: 更改桌面版日曆程式碼 */
     });
 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,10 +5,22 @@ let router = express.Router();
 let cookieParser = require('cookie-parser');
 router.use(cookieParser('PurpleRed is awesome!!'));
 
+/* INFO: MySQL */
+let mysql = require('mysql');
+dbOption = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+};
+
 router.get('/', (req, res) => {
     if (Object.keys(req.cookies).length != process.env.NUM_OF_COOKIES) {
         res.redirect('/login');
     } else {
+        let dbConnection = mysql.createConnection(dbOption);
+
         res.render('dashboard', {
             username: req.cookies.username
         });
