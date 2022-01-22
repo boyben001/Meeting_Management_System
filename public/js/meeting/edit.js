dcbfa656b9894f0f0e9f52526f713bda10d9cf0a
PurpleRed <benson060211@gmail.com>
2022-01-22 13:15:57 +0800

    feat: /meeting/edit 動態新增討論事項

--- public/js/meeting/edit.js
+++ public/js/meeting/edit.js
@@ -1,4 +1,5 @@
-window.onload = function() {
+$(document).ready(() => {
+    /* INFO: 身分權限 */
     let form = document.getElementById('authority');
     for (let i in form.childNodes) {
         if (i % 2 == 1) {
@@ -24,4 +25,45 @@ window.onload = function() {
             });
         }
     }
-}
+
+    /* INFO: 動態新增刪除提案 */
+    let disscussNum = 0;
+    $('#add_discuss_btn').click(() => {
+        disscussNum += 1;
+        let item = 'discuss' + disscussNum;
+        $(`
+            <div id="${item}" class="card">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <h3>提案</h3>
+                <button type="button" id="delete_${item}_btn" class="icon_text_button">
+                  <span class="material-icons">delete</span>
+                  <span>刪除</span>
+                </button>
+              </div>
+              <div class="card_item">
+                <label>案&emsp;&emsp;由</label>
+                <textarea id="${item}_title" name="${item}Title" rows="3"></textarea>
+              </div>
+              <div class="card_item">
+                <label>說&emsp;&emsp;明</label>
+                <textarea id="${item}_content" name="${item}Content" rows="6"></textarea>
+              </div>
+              <div class="card_item">
+                <label>決議事項</label>
+                <textarea id="${item}_resolution" name="${item}Resolution" rows="6"></textarea>
+              </div>
+              <div class="card_item">
+                <label>執行情況</label>
+                <textarea id="${item}_implementation" name="${item}Implementation" rows="6"></textarea>
+              </div>
+            </div>
+        `).hide().appendTo('#discuss_container').fadeIn(1000);
+        $('html, body').animate({
+            scrollTop: $('#' + item).offset().top - 100
+        }, 300);
+
+        $('#delete_' + item + '_btn').click(() => {
+            $('#' + item).fadeOut(1000, () => $('#' + item).remove());
+        });
+    });
+});
