5436c0326920813d6ab99333ae9d81a51d5cc01e
DESKTOP-BENCHEN\Ben-Chen <boyben001@gmail.com>
2022-01-23 11:52:01 +0800

    fixed : delete Modal 改成 淡入淡出

--- public/js/overview.js
+++ public/js/overview.js
@@ -1,1 +1,27 @@
+$(document).ready(() => {
+    $(window).resize(() => {
+        var wdth = $(window).width();
+        if (wdth > 768) {
+            $("#meeting_viewer").css("display", "flex");
+        } else {
+            $("#meeting_viewer").css("display", "none");
+        }
+    });
+    $('.card_content').click(() => {
+        if (window.innerWidth <= 768) {
+            $('#meeting_list').hide();
+            $('#meeting_viewer').fadeIn();
 
+        } else if (window.innerWidth <= 768) {
+            $('#meeting_viewer').hide();
+            $('#meeting_list').fadeIn();
+
+        }
+    });
+    $('.back').click(() => {
+        if (window.innerWidth <= 768) {
+            $('#meeting_viewer').hide();
+            $('#meeting_list').show();
+        }
+    });
+});
